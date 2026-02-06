07b9e4154adbce884ec25a84b44f1fc80ddf045c
LucaStellato <lucastellato435@gmail.com>
2026-02-06 10:36:00 +0100

    snack 4 complete

---
+++ jsnack4/snack4.js
@@ -0,0 +1,18 @@
+/*
+Crea un generatore di funzioni creaTimer
+Scrivi una funzione creaTimer che accetta un tempo (in ms)
+ e restituisce una nuova funzione che avvia un setTimeout per stampare 
+ "Tempo scaduto!".
+ */
+
+function creaTimer(tempo) {
+    return () => {
+        setTimeout(() => {
+            console.log('Tempo scaduto!')
+        }, tempo
+        )
+    }
+}
+
+const timer2s = creaTimer(2000)
+timer2s()
